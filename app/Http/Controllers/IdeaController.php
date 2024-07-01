7de49ab8c59b6c84dccdef8eb5d042a663cd3f3b
Suleiman Odetoro <odetoro75@gmail.com>
2024-07-01 20:52:18 +0100

    add validaiton rules for idea creation

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -15,6 +15,10 @@ class IdeaController extends Controller
 
     public function store()
     {
+        request()->validate([
+            'idea' => 'required|min:5|max:245',
+
+        ]);
         $idea = Idea::create([
             'content' => request()->get('idea', ''),
             'likes' => 0,
